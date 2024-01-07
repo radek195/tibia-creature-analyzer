34c65a5b9a7353b764c43884dc923cafd53e7e96
Radek <radoslaw.parol95@gmail.com>
2024-01-07 20:44:36 +0100

    add SoloHunt pojo

---
+++ src/main/java/com/example/domain/helpers/NameQuantityPair.java
@@ -0,0 +1,6 @@
+package com.example.domain.helpers;
+
+public class NameQuantityPair {
+    String name;
+    int amount;
+}
---
+++ src/main/java/com/example/domain/solohunt/SoloHunt.java
@@ -0,0 +1,12 @@
+package com.example.domain.solohunt;
+
+import com.example.domain.helpers.NameQuantityPair;
+
+public class SoloHunt {
+    Long id;
+    int balance;
+    NameQuantityPair[] killedMonsters;
+    int loot;
+    NameQuantityPair[] lootedItems;
+    int supplies;
+}
