c71b65a6becb1bfbe83c03c28ab6bb83e7c15e69
Radek <radoslaw.parol95@gmail.com>
2024-01-07 20:44:06 +0100

    add class connecting to database

---
+++ src/main/java/com/example/database/DbConnection.java
@@ -0,0 +1,33 @@
+package com.example.database;
+
+import java.sql.*;
+
+public class DbConnection {
+    private final Connection connection;
+
+    public DbConnection() {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "password";
+
+        try {
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(
+                    String.format("Could not connect to database with url: %s, username: %s, password: %s", url, username, password)
+            );
+        }
+    }
+
+    public PreparedStatement createPreparedStatement(String query) throws SQLException {
+        return connection.prepareStatement(query);
+    }
+
+    public ResultSet executeQuery(PreparedStatement statement) throws SQLException {
+        return statement.executeQuery();
+    }
+
+    public void executeUpdate(PreparedStatement statement) throws SQLException {
+        statement.executeUpdate();
+    }
+}
