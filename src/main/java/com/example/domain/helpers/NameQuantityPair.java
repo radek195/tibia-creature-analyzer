a902d9d7336e6dbf6609b1ba38542a35de842e17
Radek <radoslaw.parol95@gmail.com>
2024-01-14 16:34:41 +0100

    add method for creating json string from array

--- src/main/java/com/example/domain/helpers/NameQuantityPair.java
+++ src/main/java/com/example/domain/helpers/NameQuantityPair.java
@@ -1,6 +1,28 @@
 package com.example.domain.helpers;
 
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+@Data
 public class NameQuantityPair {
-    String name;
-    int amount;
+    private final String name;
+    private final int quantity;
+
+    public static String getJsonString(NameQuantityPair[] arr) {
+        StringBuilder jsonString = new StringBuilder("[");
+        Iterator<NameQuantityPair> it = Arrays.stream(arr).iterator();
+
+        while (it.hasNext()) {
+            NameQuantityPair pair = it.next();
+            String jsonObject = String.format("{%s: %s}", pair.getName(), pair.getQuantity());
+            jsonString.append(jsonObject);
+            if (it.hasNext()) {
+                jsonString.append(",");
+            }
+        }
+        jsonString.append("]");
+        return jsonString.toString();
+    }
 }
