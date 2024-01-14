67d7a5f6768db786285e1b9e20a796524f747223
Radek <radoslaw.parol95@gmail.com>
2024-01-14 19:41:18 +0100

    add mapping helper

---
+++ src/main/java/com/example/database/MappingHelper.java
@@ -0,0 +1,33 @@
+package com.example.database;
+
+import com.example.domain.helpers.CountNamePair;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class MappingHelper {
+    @Getter
+    private final ObjectMapper mapper;
+
+    public String from(List<CountNamePair> pairs) {
+        try {
+            return mapper.writeValueAsString(pairs);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Could not map object to string. \n %s", e);
+        }
+    }
+
+    public List<CountNamePair> jsonToList(String json) {
+        try {
+            return mapper.readValue(json, new TypeReference<List<CountNamePair>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Could not map string to object. \n %s", e);
+        }
+    }
+}
