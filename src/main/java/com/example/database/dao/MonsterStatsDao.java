d90089355549f898942df13da4ece41df0221a43
Radek <radoslaw.parol95@gmail.com>
2024-01-27 20:49:52 +0100

    implement MonsterStatsDao

--- src/main/java/com/example/database/dao/MonsterStatsDao.java
+++ src/main/java/com/example/database/dao/MonsterStatsDao.java
@@ -5,6 +5,7 @@ import com.example.domain.monsterstats.MonsterStats;
 import lombok.RequiredArgsConstructor;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -32,16 +33,36 @@ public class MonsterStatsDao implements Dao<MonsterStats> {
 
     @Override
     public Optional<MonsterStats> get(long id) throws SQLException {
-        return Optional.empty();
+        String query = String.format("SELECT * FROM %s.monster_stats WHERE id = ?", SCHEMA);
+        PreparedStatement statement = dbConnection.createPreparedStatement(query);
+        statement.setLong(1, id);
+
+        ResultSet rs = dbConnection.executeQuery(statement);
+
+        return Optional.ofNullable(MonsterStats.from(rs));
     }
 
     @Override
     public void update(MonsterStats object, long id) throws SQLException {
+        String query = String.format("UPDATE %s.monster_stats SET name = ?, amountKilled = ?, avgLoot = ?, totalLoot = ?, avgBalance = ?, avgSupplies = ? WHERE id = ?", SCHEMA);
+        PreparedStatement statement = dbConnection.createPreparedStatement(query);
+        statement.setString(1, object.getName());
+        statement.setInt(2, object.getAmountKilled());
+        statement.setInt(3, object.getAvgLoot());
+        statement.setInt(4, object.getTotalLoot());
+        statement.setInt(5, object.getAvgBalance());
+        statement.setInt(6, object.getAvgSupplies());
+        statement.setLong(7, id);
 
+        dbConnection.executeUpdate(statement);
     }
 
     @Override
     public void delete(long id) throws SQLException {
+        String query = String.format("DELETE FROM %s.monster_stats WHERE id = ?", SCHEMA);
+        PreparedStatement statement = dbConnection.createPreparedStatement(query);
+        statement.setLong(1, id);
 
+        dbConnection.executeQuery(statement);
     }
 }
